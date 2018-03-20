55ea7f68029213e6fac6dd24dee2108f15e7b622
49AARON <33808794+49AARON@users.noreply.github.com>
2018-03-20 14:42:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ const getImageStreamFromMessage = (message) => {
 
 const handleSuccessResponse = (session, caption) => {
     if (caption) {
-        session.send('I think it\'s ' + caption);
+        session.send('มันคืออออ \'s ' + caption);
     } else {
         session.send('Couldn\'t find a caption for this one');
     }
